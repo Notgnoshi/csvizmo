8d01ea6fca420747a39fcbac23f2222389838ea7
Austin Gill <Notgnoshi@gmail.com>
2025-02-13 21:16:52 -0600

    Enable test logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
 pub mod can;
 pub mod stdio;
+
+#[cfg(test)]
+#[ctor::ctor]
+fn setup_test_logging() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::TRACE)
+        .with_test_writer()
+        .with_ansi(true)
+        .init();
+}
