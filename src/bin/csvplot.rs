20f0b35ffce460953056d847397ca87885be338b
Austin Gill <Notgnoshi@gmail.com>
2025-02-23 14:55:31 -0600

    Add axes2d settings, except for plot color

--- src/bin/csvplot.rs
+++ src/bin/csvplot.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use csvizmo::csv::{column_index, exit_after_first_failed_read, parse_multi_columns};
 use csvizmo::stdio::get_input_reader;
+use gnuplot::AxesCommon;
 
 /// Plot data from a CSV file
 ///
@@ -20,6 +21,10 @@ struct Args {
     #[clap(short, long, default_value_t = tracing::Level::INFO)]
     log_level: tracing::Level,
 
+    /// Dump gnuplot commands to stdout
+    #[clap(short, long)]
+    verbose: bool,
+
     /// Path to the input. stdin if '-' or if not passed
     input: Option<PathBuf>,
 
@@ -35,13 +40,14 @@ struct Args {
     #[clap(long)]
     scatter: bool,
 
-    /// Filter the x axis range to just the given inclusive range
-    #[clap(long, number_of_values = 2, value_names = ["XMIN", "XMAX"])]
-    xlim: Vec<f64>,
-
-    /// Filter the y axis range to just the given inclusive range
-    #[clap(long, number_of_values = 2, value_names = ["YMIN", "YMAX"])]
-    ylim: Vec<f64>,
+    #[clap(long)]
+    xmin: Option<f64>,
+    #[clap(long)]
+    xmax: Option<f64>,
+    #[clap(long)]
+    ymin: Option<f64>,
+    #[clap(long)]
+    ymax: Option<f64>,
 
     /// The X column name or index. If not specified, the Y column will be treated as a time series
     #[clap(short, value_name = "X COLUMN")]
@@ -78,34 +84,92 @@ fn main() -> eyre::Result<()> {
         .delimiter(args.delimiter as u8)
         .from_reader(input);
 
+    let header = reader.headers()?.clone();
+
+    let mut ynames = Vec::new();
     let mut column_indices = Vec::new();
     for y in args.y {
         let idx = column_index(&mut reader, y)?;
+        if has_header {
+            ynames.push(header.get(idx).unwrap());
+        }
+
         column_indices.push(idx);
     }
+    let mut xname = "";
     // Put the X axis at the end, so that it's easier to add/remove if the X axis isn't specified.
     if let Some(x) = &args.x {
         let idx = column_index(&mut reader, x)?;
+        if has_header {
+            xname = header.get(idx).unwrap();
+        }
         column_indices.push(idx);
     }
     let records = exit_after_first_failed_read(reader.into_records());
     let mut data = parse_multi_columns(records, &column_indices);
     assert!(!data.is_empty());
 
+    let mut fig = gnuplot::Figure::new();
+    let axes = fig.axes2d();
+
     if args.x.is_none() {
         tracing::info!("No X-axis column given. Proceeding with time series");
+        xname = "index";
         let indices: Vec<_> = (0..data[0].len()).map(|i| i as f64).collect();
         data.push(indices);
     }
 
     let xs = data.remove(data.len() - 1);
-    let mut fig = gnuplot::Figure::new();
-    for ys in data {
-        fig.axes2d().lines(xs.iter(), ys, &[]);
+
+    for (ys, yname) in data.iter().zip(ynames) {
+        // If there's only one Y column, then label the Y axis, otherwise rely on the legend
+        if data.len() == 1 {
+            axes.set_y_label(yname, &[]);
+        }
+
+        let plotter = if args.scatter {
+            gnuplot::Axes2D::points
+        } else {
+            gnuplot::Axes2D::lines
+        };
+
+        // TODO: Color each column differently
+        // TODO: Try to mirror seaborn's default style
+        let mut options = Vec::new();
+        if has_header {
+            options.push(gnuplot::PlotOption::Caption(yname));
+        }
+        plotter(axes, xs.iter(), ys, &options);
     }
-    // TODO: xlim,ylim
-    // TODO: Scatter
-    // TODO: Legend, title, axis labels
+
+    // TODO: Disable LaTeX label formatting?
+    axes.set_x_label(xname, &[]);
+    axes.set_x_range(
+        args.xmin
+            .map(gnuplot::AutoOption::Fix)
+            .unwrap_or(gnuplot::AutoOption::Auto),
+        args.xmax
+            .map(gnuplot::AutoOption::Fix)
+            .unwrap_or(gnuplot::AutoOption::Auto),
+    );
+    axes.set_y_range(
+        args.ymin
+            .map(gnuplot::AutoOption::Fix)
+            .unwrap_or(gnuplot::AutoOption::Auto),
+        args.ymax
+            .map(gnuplot::AutoOption::Fix)
+            .unwrap_or(gnuplot::AutoOption::Auto),
+    );
+    axes.set_x_grid(true);
+    axes.set_y_grid(true);
+
+    if let Some(path) = args.input {
+        let name = path.file_stem().unwrap().to_string_lossy();
+        fig.set_title(&name);
+    }
+    if args.verbose {
+        fig.echo(&mut std::io::stdout());
+    }
     fig.show()?;
 
     Ok(())
