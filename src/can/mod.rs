6aaf6f6cecd64da51ffdd5fef5e8433b8fe17667
Austin Gill <Notgnoshi@gmail.com>
2025-07-25 10:06:44 -0500

    can2k: Optionally format GPS location as WKT

--- src/can/mod.rs
+++ src/can/mod.rs
@@ -8,6 +8,6 @@ mod tp;
 pub use candump::{CandumpFormat, CandumpParser, parse_candump};
 pub use fastpacket::FastPacketSession;
 pub use frame::{CanFrame, CanMessage};
-pub use nmea2000::{N2kParser, parse_n2k_gps};
+pub use nmea2000::{GpsData, GpsDataWkt, N2kParser, parse_n2k_gps};
 pub use session::{Session, SessionManager, reconstruct_transport_sessions};
 pub use tp::Iso11783TransportProtocolSession;
