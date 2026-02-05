5c2798e4fdfdbe018e6fe43b330deede28ffe144
Austin Gill <Notgnoshi@gmail.com>
2026-02-05 17:55:42 -0600

    Add placeholder depconv binary and integration test

---
+++ crates/csvizmo-depgraph/tests/depconv.rs
@@ -0,0 +1,10 @@
+use csvizmo_test::{CommandExt, tool};
+use pretty_assertions::assert_eq;
+
+#[test]
+fn empty_input() {
+    let output = tool!("depconv").captured_output().unwrap();
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    assert!(output.status.success());
+    assert_eq!(stdout, "");
+}
